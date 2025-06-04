63d424b30c44483c614617570bb19b2f8a414782
khalix4 <jeremiah.abu@binghamuni.edu.ng>
2025-06-04 14:00:49 +0100

    added files

---
+++ database/mrfoms_db.sql
@@ -0,0 +1,452 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 01, 2025 at 09:48 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mrfoms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_list`
+--
+
+CREATE TABLE `cart_list` (
+  `id` int(30) NOT NULL,
+  `client_id` int(30) NOT NULL,
+  `product_id` int(30) NOT NULL,
+  `quantity` float NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_list`
+--
+
+CREATE TABLE `category_list` (
+  `id` int(30) NOT NULL,
+  `vendor_id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category_list`
+--
+
+INSERT INTO `category_list` (`id`, `vendor_id`, `name`, `description`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(11, 5, 'Munch Box Food', 'Food', 1, 0, '2025-06-01 20:16:22', '2025-06-01 20:28:27'),
+(12, 6, 'Nabis Food', 'Food', 1, 0, '2025-06-01 20:24:45', '2025-06-01 20:27:42'),
+(13, 7, 'Omega Food', 'Omega Food', 1, 0, '2025-06-01 20:46:09', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_list`
+--
+
+CREATE TABLE `client_list` (
+  `id` int(30) NOT NULL,
+  `code` varchar(100) NOT NULL,
+  `firstname` text NOT NULL,
+  `middlename` text DEFAULT NULL,
+  `lastname` text NOT NULL,
+  `gender` text NOT NULL,
+  `contact` text NOT NULL,
+  `address` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `client_list`
+--
+
+INSERT INTO `client_list` (`id`, `code`, `firstname`, `middlename`, `lastname`, `gender`, `contact`, `address`, `email`, `password`, `avatar`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(3, '202506-00001', 'Samuel', '', 'Olubukun', 'Male', '08083273795', 'FCT Abuja', 'samuelolubukun@gmail.com', '91e92ac16226d35d8ff1f7e65553e858', 'uploads/clients/3.png?v=1748806285', 1, 0, '2025-06-01 20:31:25', '2025-06-01 20:31:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `order_id` int(30) NOT NULL,
+  `product_id` int(30) NOT NULL,
+  `quantity` double NOT NULL DEFAULT 0,
+  `price` double NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`order_id`, `product_id`, `quantity`, `price`, `date_created`) VALUES
+(5, 10, 1, 4000, '2025-06-01 20:32:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_list`
+--
+
+CREATE TABLE `order_list` (
+  `id` int(30) NOT NULL,
+  `code` varchar(100) NOT NULL,
+  `client_id` int(30) NOT NULL,
+  `vendor_id` int(30) NOT NULL,
+  `total_amount` double NOT NULL DEFAULT 0,
+  `delivery_address` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_list`
+--
+
+INSERT INTO `order_list` (`id`, `code`, `client_id`, `vendor_id`, `total_amount`, `delivery_address`, `status`, `date_created`, `date_updated`) VALUES
+(5, '202506-00001', 3, 5, 4000, 'FCT Abuja', 0, '2025-06-01 20:32:53', '2025-06-01 20:32:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_list`
+--
+
+CREATE TABLE `product_list` (
+  `id` int(30) NOT NULL,
+  `vendor_id` int(30) DEFAULT NULL,
+  `category_id` int(30) DEFAULT NULL,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `price` double NOT NULL DEFAULT 0,
+  `image_path` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_list`
+--
+
+INSERT INTO `product_list` (`id`, `vendor_id`, `category_id`, `name`, `description`, `price`, `image_path`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(10, 5, 11, 'Jollof Rice', '&lt;p&gt;Jollof Rice&lt;/p&gt;', 4000, 'uploads/products/10.png?v=1748805436', 1, 0, '2025-06-01 20:17:16', '2025-06-01 20:17:16'),
+(11, 5, 11, 'Spaghetti', '&lt;h4 style=&quot;margin-right: 0px; margin-bottom: 0px; margin-left: 0px; line-height: 19.2px; position: relative; padding: 0px 0px 10px;&quot;&gt;&lt;font color=&quot;#ffffff&quot; face=&quot;Rubik, sans-serif&quot;&gt;&lt;span style=&quot;font-size: 16px;&quot;&gt;Spaghetti&lt;/span&gt;&lt;/font&gt;&lt;/h4&gt;', 3000, 'uploads/products/11.png?v=1748805538', 1, 0, '2025-06-01 20:18:58', '2025-06-01 20:18:58'),
+(12, 6, 12, 'Jollof Rice with Grilled Chicken', '&lt;h4 style=&quot;margin-right: 0px; margin-bottom: 0px; margin-left: 0px; font-family: Rubik, sans-serif; line-height: 19.2px; color: rgb(255, 255, 255); font-size: 16px; position: relative; padding: 0px 0px 10px;&quot;&gt;Jollof Rice with Grilled Chicken&lt;/h4&gt;', 6000, 'uploads/products/12.png?v=1748805930', 1, 0, '2025-06-01 20:25:30', '2025-06-01 20:25:30'),
+(13, 6, 12, 'White Rice and Stew', '&lt;h4 style=&quot;margin-right: 0px; margin-bottom: 0px; margin-left: 0px; font-family: Rubik, sans-serif; line-height: 19.2px; color: rgb(255, 255, 255); font-size: 16px; position: relative; padding: 0px 0px 10px;&quot;&gt;White Rice and Stew&lt;/h4&gt;', 2500, 'uploads/products/13.png?v=1748805972', 1, 0, '2025-06-01 20:26:12', '2025-06-01 20:26:12'),
+(14, 7, 13, 'Fried Rice and Chicken', '&lt;p&gt;Fried Rice and Chicken&lt;/p&gt;', 3500, 'uploads/products/14.png?v=1748807209', 1, 0, '2025-06-01 20:46:49', '2025-06-01 20:46:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop_type_list`
+--
+
+CREATE TABLE `shop_type_list` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `shop_type_list`
+--
+
+INSERT INTO `shop_type_list` (`id`, `name`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(6, 'Cafeteria', 1, 0, '2025-06-01 20:11:24', '2025-06-01 20:12:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', 'Multi-Restaurant Food Ordering Management System'),
+(6, 'short_name', 'MRFOMS - PHP'),
+(11, 'logo', 'uploads/logo-1748804594.png'),
+(13, 'user_avatar', 'uploads/user_avatar.jpg'),
+(14, 'cover', 'uploads/cover-1748804674.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
+(1, 'Adminstrator', 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/avatar-1.png?v=1644472635', NULL, 1, '2021-01-20 14:02:37', '2022-02-10 13:57:15'),
+(11, 'Claire', 'Blake', 'cblake', '4744ddea876b11dcb1d169fadf494418', 'uploads/avatar-11.png?v=1644472553', NULL, 2, '2022-02-10 13:55:52', '2022-02-10 13:55:53');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendor_list`
+--
+
+CREATE TABLE `vendor_list` (
+  `id` int(30) NOT NULL,
+  `code` varchar(100) NOT NULL,
+  `shop_type_id` int(30) NOT NULL,
+  `shop_name` text NOT NULL,
+  `shop_owner` text NOT NULL,
+  `contact` text NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vendor_list`
+--
+
+INSERT INTO `vendor_list` (`id`, `code`, `shop_type_id`, `shop_name`, `shop_owner`, `contact`, `username`, `password`, `avatar`, `status`, `delete_flag`, `date_created`, `date_updated`) VALUES
+(5, '202506-00001', 6, 'Munch Box', 'Munch Box', '0000000000', 'MunchBox', 'a1ca37cba4b7773e13d0fc913fea9910', 'uploads/vendors/5.png?v=1748805290', 1, 0, '2025-06-01 20:14:50', '2025-06-01 20:14:50'),
+(6, '202506-00002', 6, 'Nabis', 'Nabis', '0000000000', 'Nabis', 'c189367b34f2bd547912e14727d82fba', 'uploads/vendors/6.png?v=1748805810', 1, 0, '2025-06-01 20:23:30', '2025-06-01 20:23:30'),
+(7, '202506-00003', 6, 'Omega', 'Omega', '0000000000', 'Omega', 'c6d6bd7ebf806f43c76acc3681703b81', 'uploads/vendors/7.png?v=1748807075', 1, 0, '2025-06-01 20:44:35', '2025-06-01 20:44:35');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart_list`
+--
+ALTER TABLE `cart_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `category_list`
+--
+ALTER TABLE `category_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vendor_id` (`vendor_id`);
+
+--
+-- Indexes for table `client_list`
+--
+ALTER TABLE `client_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `vendor_id` (`vendor_id`);
+
+--
+-- Indexes for table `product_list`
+--
+ALTER TABLE `product_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `vendor_id` (`vendor_id`),
+  ADD KEY `category_id` (`category_id`) USING BTREE;
+
+--
+-- Indexes for table `shop_type_list`
+--
+ALTER TABLE `shop_type_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `vendor_list`
+--
+ALTER TABLE `vendor_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `shop_type_id` (`shop_type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart_list`
+--
+ALTER TABLE `cart_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `category_list`
+--
+ALTER TABLE `category_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `client_list`
+--
+ALTER TABLE `client_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `order_list`
+--
+ALTER TABLE `order_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `product_list`
+--
+ALTER TABLE `product_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `shop_type_list`
+--
+ALTER TABLE `shop_type_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `system_info`
+--
+ALTER TABLE `system_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `vendor_list`
+--
+ALTER TABLE `vendor_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart_list`
+--
+ALTER TABLE `cart_list`
+  ADD CONSTRAINT `cart_list_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cart_list_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `category_list`
+--
+ALTER TABLE `category_list`
+  ADD CONSTRAINT `category_list_ibfk_1` FOREIGN KEY (`vendor_id`) REFERENCES `vendor_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD CONSTRAINT `order_list_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client_list` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_list_ibfk_2` FOREIGN KEY (`vendor_id`) REFERENCES `vendor_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `product_list`
+--
+ALTER TABLE `product_list`
+  ADD CONSTRAINT `product_list_ibfk_1` FOREIGN KEY (`vendor_id`) REFERENCES `vendor_list` (`id`) ON DELETE SET NULL,
+  ADD CONSTRAINT `product_list_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category_list` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `vendor_list`
+--
+ALTER TABLE `vendor_list`
+  ADD CONSTRAINT `vendor_list_ibfk_1` FOREIGN KEY (`shop_type_id`) REFERENCES `shop_type_list` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
